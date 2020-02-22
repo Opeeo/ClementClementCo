198ec9abae297ef50d94343909547954d7eb9034
Clément Albos <44971574+Xompre@users.noreply.github.com>
2020-02-22 21:53:04 +0100

    Modules

--- React/hichatreact/src/App.js
+++ React/hichatreact/src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    data: null 
+  };
+
+  handleClick = () => {
+    axios
+    .get('https://localhost:8080/api/users/recupInfo')
+    .then(data => (this.setState({data: data})))
+    .catch(err => (console.log(err)))
+    console.log(this.state);
+  }
+
+  render() {
+
+    const title = "Test Page de Profil Utilisateur";
+
+    return(
+      <div>
+         <h1>{title}</h1>
+        
+         <button onClick={this.handleClick}>Catch it bru</button>
+
+
+      </div>
+  
+    );   
+  }
 }
+
+
 
 export default App;
